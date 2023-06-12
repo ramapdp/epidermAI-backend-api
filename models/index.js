2f011d98b94d463150dba64197222e5db776cdc7
ramapdp <ramasatelit484@gmail.com>
2023-06-13 00:35:11 +0700

    fix process.env.DB_USERNAME

--- models/index.js
+++ models/index.js
@@ -12,7 +12,7 @@ const db = {};
 
 const sequelize = new Sequelize(
   process.env.DB_NAME,
-  process.env.DB_USER,
+  process.env.DB_USERNAME,
   process.env.DB_PASSWORD,
   {
     dialect: "mysql",
