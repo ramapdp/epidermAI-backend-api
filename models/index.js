d9824ad6a2e67422e3bb440f3da67b402d0a2fd9
ramapdp <ramasatelit484@gmail.com>
2023-06-13 00:14:30 +0700

    fix index.js in models

--- models/index.js
+++ models/index.js
@@ -7,34 +7,48 @@ const process = require('process');
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../config/config')[env];
+require('dotenv').config();
 const db = {};
 
-let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
-} else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
-  // sequelize = new Sequelize(
-  //   config.database,
-  //   config.username,
-  //   config.password,
-  //   {
-  //     dialect: 'mysql',
-  //     host: 34.101.197.94,
-  //     pool: {
-  //         max: 5,
-  //         min: 0,
-  //         acquire: 30000,
-  //         idle: 10000
-  //     },
-  //     dialectOptions: {
-  //         socketPath: '/cloudsql/adhaar-351813:us-central1:adhaar-sql'
-  //     },
-  //     logging: false,
-  //     operatorsAliases: false
-  // }
-  // );
-}
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    dialect: "mysql",
+    host: process.env.DB_HOST,
+    dialectOptions: {
+      socketPath: process.env.DB_LINK
+    },
+  }
+);
+
+// let sequelize;
+// if (config.use_env_variable) {
+//   sequelize = new Sequelize(process.env[config.use_env_variable], config);
+// } else {
+//   sequelize = new Sequelize(config.database, config.username, config.password, config);
+//   sequelize = new Sequelize(
+//     config.database,
+//     config.username,
+//     config.password,
+//     {
+//       dialect: 'mysql',
+//       host: 34.101.197.94,
+//       pool: {
+//           max: 5,
+//           min: 0,
+//           acquire: 30000,
+//           idle: 10000
+//       },
+//       dialectOptions: {
+//           socketPath: '/cloudsql/adhaar-351813:us-central1:adhaar-sql'
+//       },
+//       logging: false,
+//       operatorsAliases: false
+//   }
+//   );
+// }
 
 fs
   .readdirSync(__dirname)
